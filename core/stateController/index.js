c0c2df796c03b765080d68490eaf02cdcaf6d812
Terry-Su <2684759855@qq.com>
2017-09-27 23:47:06 +0800

    Tmp commit

---
+++ core/stateController/index.js
@@ -0,0 +1,28 @@
+const pathStateController = require('./path/index')
+
+function init() {
+  global.mvvcStateControllers = {
+
+  }
+
+  // state
+  mvvcStateControllers = Object.assign(mvvcStateControllers, {
+    pathStateController
+  })
+
+  // mutation
+  let { mutation } = pathStateController
+  const keys = Object.keys(mutation)
+  const fns = Object.values(mutation)
+  fns.map((fn, index) => {
+    if(typeof fn === 'function') {
+      mutation[index] = (state, param) => fn(pathStateController.state, param)
+    }
+  })
+
+  // actions
+
+
+  // getters
+
+}
---
+++ core/stateController/path/index.js
@@ -0,0 +1,26 @@
+const state = {
+
+}
+
+const mutation = {
+  updateState(state, param) {
+
+  }
+}
+
+const actions = {
+  UPDATE_STATE(context, param) {
+    context.commit('updateState', param)
+  }
+}
+
+const getters = {
+
+}
+
+module.exports = {
+  state,
+  mutation,
+  actions,
+  getters
+}
